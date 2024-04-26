b4e3bb32ed1a9f1a781cdfae1071fe1ff460d1f7
davidweslley <davidweslleymartins@gmail.com>
2024-04-26 19:21:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ form.addEventListener('submit', function (e) {
 	const input_num_a = document.getElementById('na');
 	const input_num_b = document.getElementById('nb');
 
-	const msgOK = `SIM - ${input_num_a.value} é maior que número A}`;
-	const msgNot = `NÃO - ${input_num_a.value} NÃO é maior que número B`;
+	const msgOK = `SIM - ${input_num_a.value} é maior que ${input_num_b.value}`;
+	const msgNot = `NÃO - ${input_num_a.value} NÃO é maior que ${input_num_b.value}`;
 
 	if (input_num_a.value > input_num_b.value) {
 		alert(msgOK);
